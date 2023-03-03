f35b8c1d0b5dcd33ecad861c65b61b8a853726cb
devhiago <114820188+Hiago04@users.noreply.github.com>
2023-03-02 22:45:55 -0300

    organizando pastas + revisando

--- seção-10/aula 9/revisao/routes.js
+++ seção-10/aula 9/revisao/routes.js
@@ -1,12 +1,10 @@
-const express = require('express')
+const express = require('express');
 const route = express.Router()
 const homeController = require('./controllers/homeController')
 const contatoController = require('./controllers/contatoController')
 
-
-// Rotas da home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost)
+route.get('/', homeController.homePage);
+route.post('/', homeController.trataPost);
 
 route.get('/contato', contatoController.paginaInicial);
 
