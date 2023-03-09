dba677cd3df60d5b12d91178b320fd3d58fd166c
devhiago <114820188+Hiago04@users.noreply.github.com>
2023-03-09 17:53:35 -0300

    criando a sec 11

---
+++ seção-11/projeto-agenda/src/controllers/loginController.js
@@ -0,0 +1,54 @@
+const Login = require('../models/LoginModel')
+
+exports.index = (req, res) => {
+    res.render('login');
+}
+
+exports.register = async function(req, res){
+    try {
+        const login = new Login(req.body)
+        await login.register()
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(function() {
+                return res.redirect('/login/index')
+            });
+            return
+        }
+    
+        req.flash('sucess', 'Seu usuário foi criado com sucesso');
+        req.session.save(function() {
+            return res.redirect('/login/index')
+        });
+    
+    } catch(e) {
+        console.log(e);
+        return res.render('erro.ejs')
+    }
+}
+
+exports.login = async function(req, res){
+    try {
+        const login = new Login(req.body)
+        await login.login()
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(function() {
+                return res.redirect('/login/index')
+            });
+            return
+        }
+    
+        req.flash('sucess', 'Seu usuário foi criado com sucesso');
+        req.session.save(function() {
+            return res.redirect('/login/index')
+        });
+    
+    } catch(e) {
+        console.log(e);
+        return res.render('erro.ejs')
+    }
+}
+
